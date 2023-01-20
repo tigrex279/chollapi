81e2f5e63366a6688c9df3797acdbcd6702c0b00
tigrex279 <tigrecito279@hotmail.es>
2023-01-20 13:20:35 +0100

    Dao entidades

--- chollapi/src/main/java/aad/Francisco/persistencia/GenericDAO.java
+++ chollapi/src/main/java/aad/Francisco/persistencia/GenericDAO.java
@@ -3,5 +3,5 @@ package aad.Francisco.persistencia;
 import java.io.Serializable;
 
 public interface GenericDAO<T, ID extends Serializable> {
-
+	
 }
---
+++ chollapi/src/main/java/aad/Francisco/persistencia/ProductoDAO.java
@@ -0,0 +1,5 @@
+package aad.Francisco.persistencia;
+
+public class ProductoDAO {
+
+}
